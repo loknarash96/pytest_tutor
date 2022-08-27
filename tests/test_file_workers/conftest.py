193cf165d7754558b521df8e5dc66b9a46f7c9de
loknarash96 <loknarash@yandex.ru>
2022-08-27 14:46:42 +0300

    fix path in scripts

--- tests/test_file_workers/conftest.py
+++ tests/test_file_workers/conftest.py
@@ -6,7 +6,7 @@ cnt = 0
 @pytest.fixture(autouse=True,)
 def clean_text_file():
     global cnt
-    with open("tests/testfile.txt", "w"):
+    with open("testfile.txt", "w"):
         pass
     print(cnt)
     cnt += 1
--- tests/test_file_workers/test_file_workers.py
+++ tests/test_file_workers/test_file_workers.py
@@ -3,16 +3,16 @@ import pytest
 
 def create_test_data(test_data):
 
-    with open("tests/testfile.txt", "a") as f_o:
+    with open("testfile.txt", "a") as f_o:
         f_o.writelines(test_data)
 
 def test_read_from_file():
     test_data = ["one\n", "two\n", "three\n"]
     create_test_data(test_data)
-    assert test_data == read_from_file("tests/testfile.txt")
+    assert test_data == read_from_file("testfile.txt")
 
 
 def test_read_from_file2():
     test_data = ["one\n", "two\n", "three\n", "four\n"]
     create_test_data(test_data)
-    assert test_data == read_from_file("tests/testfile.txt")
+    assert test_data == read_from_file("testfile.txt")
